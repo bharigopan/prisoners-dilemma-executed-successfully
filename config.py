1b4393a97a19bbdf40489a1568c2a413c62ee30b
bharigopan <46465452+bharigopan@users.noreply.github.com>
2024-07-22 20:53:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ import os
 # Strategies selected for simulation
 selected_strategy_names = [
     'Tit for Tat', 'Always Cooperate', 'Always Defect', 'Alternate',
-    'grudger'
+    'Grudger'
 ]
 
 # Number of rounds per game
